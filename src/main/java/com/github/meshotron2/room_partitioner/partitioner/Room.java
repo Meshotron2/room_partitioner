734f08dbe1cb09f65a5d7af227237b9633929d13
Joao-Felisberto <joaofelisberto@ua.pt>
2022-01-03 20:33:28 +0000

    [CLUSTER-59] Fixed writing and reading

--- src/main/java/com/github/meshotron2/room_partitioner/partitioner/Room.java
+++ src/main/java/com/github/meshotron2/room_partitioner/partitioner/Room.java
@@ -13,9 +13,8 @@ public class Room {
 
     private final long f;
 
-    private InputStream reader;
-    private OutputStream writer;
-//    private RandomAccessFile randomWriter;
+    private DataInputStream reader;
+    private DataOutputStream writer;
 
     Room(String file, int x, int y, int z, long f) {
         this.file = file;
@@ -31,7 +30,7 @@ public class Room {
         if (file == null)
             throw new IllegalStateException("Room file name cannot be null");
 
-        this.reader = new FileInputStream(file);
+        this.reader = new DataInputStream(new FileInputStream(file));
         reader.skip(24); // skip until first char
     }
 
@@ -58,36 +57,13 @@ public class Room {
 
         Files.createFile(path);
 
-        this.writer = new FileOutputStream(file);
+        this.writer = new DataOutputStream(new FileOutputStream(file));
 
-        this.writer.write(x >> 24);
-        this.writer.write(x >> 16);
-        this.writer.write(x >> 8);
-        this.writer.write(x);
-        this.writer.write(y >> 24);
-        this.writer.write(y >> 16);
-        this.writer.write(y >> 8);
-        this.writer.write(y);
-        this.writer.write(z >> 24);
-        this.writer.write(z >> 16);
-        this.writer.write(z >> 8);
-        this.writer.write(z);
+        this.writer.writeInt(Integer.reverseBytes(x));
+        this.writer.writeInt(Integer.reverseBytes(y));
+        this.writer.writeInt(Integer.reverseBytes(z));
 
-        //        https://stackoverflow.com/questions/10686178/convert-long-to-two-int-and-vice-versa
-
-        final int f1 = (int)(f >> 32);
-        this.writer.write(f1 >> 24);
-        this.writer.write(f1 >> 16);
-        this.writer.write(f1 >> 8);
-        this.writer.write(f1);
-
-        final int f2 = (int)f;
-        this.writer.write(f2 >> 24);
-        this.writer.write(f2 >> 16);
-        this.writer.write(f2 >> 8);
-        this.writer.write(f2);
-
-        System.out.println("f: " + ((long)f1 << 32 | f2 & 0xFFFFFFFFL));
+        this.writer.writeLong(Long.reverseBytes(f));
     }
 
     public void endWrite() throws IOException {
@@ -95,46 +71,31 @@ public class Room {
     }
 
     public void writeNode(char c) throws IOException {
-        writer.write(c >> 8);
-        writer.write((byte) c);
+        writer.write(new byte[]{(byte) c, (byte) (c >> 8)});
     }
 
-//    public void startRandomWriter() {
-//        try {
-//            this.randomWriter = new RandomAccessFile(file, "r");
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
+//    public void writeNode(char c, int x, int y, int z) throws IOException {
+//        final int n = 24 + x * y * z;
+//
+//        byte[] bytes = {(byte) (c >> 8), (byte) c};
+////        System.out.println(bytes.length);
+////        writer.write(bytes, n, 2);
+//
+//        randWriter = new RandomAccessFile(file, "rw");
+//        randWriter.seek(n);
+//        randWriter.write(bytes);
+//        randWriter.close();
 //    }
 
     public static Room fromFile(String file) throws IOException {
-        final InputStream reader = new FileInputStream(file);
+        final DataInputStream reader = new DataInputStream(new FileInputStream(file));
 
-        final byte[] bs = new byte[4];
+        final int x = Integer.reverseBytes(reader.readInt());
+        final int y = Integer.reverseBytes(reader.readInt());
+        final int z = Integer.reverseBytes(reader.readInt());
 
-        int status = reader.read(bs);
-        if (status != 4) return null;
-        final int x = ((int) bs[0]) << 24 | ((int) bs[1]) << 16 | ((int) bs[2]) << 8 | ((int) bs[3]);
+        final long f = Long.reverseBytes(reader.readLong());
 
-        status = reader.read(bs);
-        if (status != 4) return null;
-        final int y = ((int) bs[0]) << 24 | ((int) bs[1]) << 16 | ((int) bs[2]) << 8 | ((int) bs[3]);
-
-        status = reader.read(bs);
-        if (status != 4) return null;
-        final int z = ((int) bs[0]) << 24 | ((int) bs[1]) << 16 | ((int) bs[2]) << 8 | ((int) bs[3]);
-
-        // TODO: 12/6/21 Simplify
-        status = reader.read(bs);
-        if (status != 4) return null;
-        final int f1 = ((int) bs[0]) << 24 | ((int) bs[1]) << 16 | ((int) bs[2]) << 8 | ((int) bs[3]);
-        status = reader.read(bs);
-        if (status != 4) return null;
-        final int f2 = ((int) bs[0]) << 24 | ((int) bs[1]) << 16 | ((int) bs[2]) << 8 | ((int) bs[3]);
-
-        final long f = (long)f1 << 32 | f2 & 0xFFFFFFFFL;
-
-        System.out.println("f: " + f);
         reader.close();
         return new Room(file, x, y, z, f);
     }
