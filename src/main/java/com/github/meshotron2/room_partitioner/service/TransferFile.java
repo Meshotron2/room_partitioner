e17866ac683fba3bbe4127134d42386cc14ac21f
vmashp <84442053+VS-UA@users.noreply.github.com>
2022-01-18 19:47:32 +0000

    Revert "Merge branch 'master' of https://github.com/Meshotron2/room_partitioner"
    This reverts commit 7e2f3c7d3339134cf2d823b4b5f848e80f5007cf, reversing
    changes made to a437372aa0735245527e7515650eef92167bad94.

--- src/main/java/com/github/meshotron2/room_partitioner/service/TransferFile.java
+++
@@ -1,8 +0,0 @@
-package com.github.meshotron2.room_partitioner.service;
-
-public interface TransferFile {
-    
-    boolean downloadFile(String localFilePath, String removeFilePath);
-
-    boolean uploadFile(String localFilePath, String removeFilePath);
-}
