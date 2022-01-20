35d25360f67ddd434dc561f678fb2fd14f3398fa
Joao-Felisberto <joaofelisberto@ua.pt>
2022-01-20 16:45:19 +0000

    Fix for previous commit

--- src/main/java/com/github/meshotron2/room_partitioner/Main.java
+++ src/main/java/com/github/meshotron2/room_partitioner/Main.java
@@ -3,20 +3,26 @@ package com.github.meshotron2.room_partitioner;
 import com.github.meshotron2.cli_utils.CLI;
 import com.github.meshotron2.room_partitioner.cli.MainMenu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.Scanner;
 
+@SpringBootApplication
 public class Main {
-    public static final String PROMPT = "> ";
 
-    public static void main(String[] args) {
+	public static final String PROMPT = "> ";
+
+	public static void main(String[] args) {
 //        System.out.println(System.getProperty("user.dir"));
-        final Scanner scanner = new Scanner(System.in);
+		SpringApplication.run(Main.class, args);
+		final Scanner scanner = new Scanner(System.in);
 
-        final CLI cli = new CLI();
-        cli.addMenu(new MainMenu(scanner));
+		final CLI cli = new CLI();
+		cli.addMenu(new MainMenu(scanner));
 
-        cli.start();
+		cli.start();
 
-        scanner.close();
-    }
+		scanner.close();
+	}
 }
