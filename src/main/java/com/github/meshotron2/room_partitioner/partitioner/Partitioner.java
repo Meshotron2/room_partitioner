e0ba354ccfb38f45bf813c50a920217018fdf839
Joao-Felisberto <joaofelisberto@ua.pt>
2022-03-13 19:40:47 +0000

    Implemented automatic partitioning, thanks to Rúben

--- src/main/java/com/github/meshotron2/room_partitioner/partitioner/Partitioner.java
+++ src/main/java/com/github/meshotron2/room_partitioner/partitioner/Partitioner.java
@@ -4,6 +4,199 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+class PartNode {
+    private int value;
+    private PartNode[] children;
+    private int maxChildCnt;
+    private int childCnt;
+
+    private int leafs = -1;
+
+    PartNode(int value, PartNode[] children, int childCnt, int maxChildCnt) {
+        this.value = value;
+        this.children = children;
+        this.childCnt = childCnt;
+        this.maxChildCnt = maxChildCnt;
+    }
+
+    void setChildCnt(int childCnt) {
+        this.children = new PartNode[childCnt];
+        this.maxChildCnt = childCnt;
+        this.childCnt = 0;
+    }
+
+    void setValue(int value) {
+        this.value = value;
+    }
+
+    PartNode addChild(PartNode node) {
+        if (childCnt == maxChildCnt)
+            return null;
+
+        children[childCnt++] = node;
+        return children[childCnt];
+    }
+
+    int getNumLeafs() {
+        if (children == null)
+            return 1;
+
+        int sum = 0;
+
+        for (int i = 0; i < childCnt; i++)
+            sum += children[i].getNumLeafs();
+
+        return sum;
+    }
+
+    private void getLeafsInternal(PartNode[] buffer) {
+        if (leafs == -1)
+            leafs = 0;
+
+        if (children == null) {
+            buffer[leafs] = this;
+            leafs++;
+            return;
+        }
+
+        for (int i = 0; i < childCnt; i++) {
+            children[i].getLeafsInternal(buffer);
+        }
+    }
+
+    PartNode[] getLeafs() {
+        final int numLeafs = getNumLeafs();
+        PartNode[] leafs = new PartNode[numLeafs];
+
+        this.leafs = 0;
+        getLeafsInternal(leafs);
+
+        return leafs;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
+
+class PartTree {
+    private final PartNode x;
+    private final PartNode y;
+    private final PartNode z;
+
+    PartTree(PartNode x, PartNode y, PartNode z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    private int findNextRoot(int n) {
+        int r = 2;
+        while (n % r != 0)
+            r++;
+
+        return r;
+    }
+
+    public void partition(int n) {
+        if (n < 2) return;
+
+        int r = findNextRoot(n);
+
+        final PartTree[] pt = new PartTree[r];
+
+        if (x.getValue() >= y.getValue()) {
+            if (x.getValue() >= z.getValue()) {
+                int rem = x.getValue() % r;
+                x.setChildCnt(r);
+                for (int i = 0; i < r; i++) {
+                    int value;
+                    if (rem > 0) {
+                        value = x.getValue() / r + 1;
+                        rem--;
+                    } else {
+                        value = x.getValue() / r;
+                    }
+
+                    final PartNode node = new PartNode(value, null, 0, 0);
+                    final PartNode newNode = x.addChild(node);
+
+                    final PartTree t = new PartTree(newNode, y, z);
+                    pt[i] = t;
+                }
+            } else {
+                int rem = z.getValue() % r;
+                z.setChildCnt(r);
+                for (int i = 0; i < r; i++) {
+                    int value;
+                    if (rem > 0) {
+                        value = z.getValue() / r + 1;
+                        rem--;
+                    } else {
+                        value = z.getValue() / r;
+                    }
+
+                    final PartNode node = new PartNode(value, null, 0, 0);
+                    final PartNode newNode = z.addChild(node);
+
+                    final PartTree t = new PartTree(x, y, newNode);
+                    pt[i] = t;
+                }
+            }
+        } else {
+            if (x.getValue() >= z.getValue() || y.getValue() >= z.getValue()) {
+                int rem = y.getValue() % r;
+                y.setChildCnt(r);
+                for (int i = 0; i < r; i++) {
+                    int value;
+                    if (rem > 0) {
+                        value = y.getValue() / r + 1;
+                        rem--;
+                    } else {
+                        value = y.getValue() / r;
+                    }
+
+                    final PartNode node = new PartNode(value, null, 0, 0);
+                    final PartNode newNode = y.addChild(node);
+
+                    final PartTree t = new PartTree(x, newNode, z);
+                    pt[i] = t;
+                }
+            } else {
+                int rem = z.getValue() % r;
+                z.setChildCnt(r);
+                for (int i = 0; i < r; i++) {
+                    int value;
+                    if (rem > 0) {
+                        value = z.getValue() / r + 1;
+                        rem--;
+                    } else {
+                        value = z.getValue() / r;
+                    }
+
+                    final PartNode node = new PartNode(value, null, 0, 0);
+                    final PartNode newNode = z.addChild(node);
+
+                    final PartTree t = new PartTree(x, y, newNode);
+                    pt[i] = t;
+                }
+            }
+        }
+    }
+
+    public PartNode getX() {
+        return x;
+    }
+
+    public PartNode getY() {
+        return y;
+    }
+
+    public PartNode getZ() {
+        return z;
+    }
+}
+
 public class Partitioner {
 
     private Room room;
@@ -11,11 +204,46 @@ public class Partitioner {
     private final int yg;
     private final int zg;
 
+    private final int n;
+    private final PartTree tree;
+
     public Partitioner(Room room, int xg, int yg, int zg) {
         this.room = room;
         this.xg = xg;
         this.yg = yg;
         this.zg = zg;
+        this.n = xg + yg + zg;
+        tree = null;
+    }
+
+    public Partitioner(Room room, int x, int y, int z, int n) {
+        this.room = room;
+        this.xg = x;
+        this.yg = y;
+        this.zg = z;
+        this.n = n;
+
+        tree = new PartTree(
+                new PartNode(x, null, 0, 0),
+                new PartNode(y, null, 0, 0),
+                new PartNode(z, null, 0, 0)
+        );
+    }
+
+    public List<Room> autoPartition() throws IOException {
+        if (tree == null) return null;
+
+        tree.partition(n);
+
+        final PartNode x = tree.getX();
+        final PartNode y = tree.getY();
+        final PartNode z = tree.getZ();
+
+        final int xDiv = x.getNumLeafs();
+        final int yDiv = y.getNumLeafs();
+        final int zDiv = z.getNumLeafs();
+
+        return new Partitioner(room, xDiv, yDiv, zDiv).partition();
     }
 
     public List<Room> partition() throws IOException {
