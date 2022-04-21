6bb4fe1e6a7ef349f42c1160d9193106cd3779cc
Immodicus <53706492+Immodicus@users.noreply.github.com>
2022-04-21 11:20:09 -0200

    remove unused import

--- src/main/java/com/github/meshotron2/room_partitioner/service/Server.java
+++ src/main/java/com/github/meshotron2/room_partitioner/service/Server.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
